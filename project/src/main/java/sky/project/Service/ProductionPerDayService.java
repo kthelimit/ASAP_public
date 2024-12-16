8d7a1c704737e31e96f69c7e2cf1a0d7f37e6c02
K <yunsky2@naver.com>
2024-12-16 17:29:11 +0900

    241216 생산계획수정시 일자별 생산수량 함께 변경
    - 정확히는 해당 생산 계획에 붙은 일자별 생산 수량을 전부 삭제한 후 다시 넣는 것이지만 어쨌든... 변경된다.
    - 이후 소요 자재 변동을 구현할 것.

--- project/src/main/java/sky/project/Service/ProductionPerDayService.java
+++ project/src/main/java/sky/project/Service/ProductionPerDayService.java
@@ -2,8 +2,14 @@ package sky.project.Service;
 
 import sky.project.DTO.ProductionPerDayDTO;
 
+import java.util.List;
+
 public interface ProductionPerDayService {
     void register(ProductionPerDayDTO dto);
 
     void delete(Long id);
+
+    List<ProductionPerDayDTO> findbyPlanId(Long planId);
+
+    ProductionPerDayDTO findById(Long id);
 }
